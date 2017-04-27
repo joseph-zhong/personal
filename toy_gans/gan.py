4867caa1bd716baf643db38ae688ff66162f9cf8
Joseph Zhong <joseph.zhong@hotmail.com>
2017-04-26 20:57:43 -0700

    update personal directory with submodules

---
+++ toy_gans/gan.py
@@ -0,0 +1,24 @@
+"""
+
+Joseph Zhong
+25 April 2017
+
+
+gan.py
+---
+
+    Introduction to GANs by training a model to
+    approximate a 1D Gaussian Distribution.
+
+
+Procedure
+---
+
+    We want to first generate our "real" dataset
+    across our Gaussian distribution
+
+
+"""
+
+# We will
+
